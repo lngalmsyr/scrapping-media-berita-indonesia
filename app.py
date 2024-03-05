1f7ede7f4e635cb1a11be67cebe4f136e0fa1dc0
Lanang Almasyuri <85623349+lngalmsyr@users.noreply.github.com>
2024-03-05 09:55:46 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+from Scrapper import run_cnn, run_kompas, run_tempo, run_turnbackhoax
+
+
+# run_cnn(num_of_page=100)
+# run_kompas(num_of_page=100)
+run_tempo(num_of_day=300, start_date='2024-01-01')
+# run_turnbackhoax(num_of_page=100)
